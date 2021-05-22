b5165e67ede98756b3444cc5eec37a83ba776ecb
Utavegu <utavegu@list.ru>
2021-05-22 14:15:32 +0500

    Начал работу над новой версией своего портфолио

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Portfolio from './Portfolio';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Portfolio />
   </React.StrictMode>,
   document.getElementById('root')
 );
